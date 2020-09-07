287959d71c90f6ba117c96e0f8ee95e6cfc9596c
Goudarz Jafari <goudarz.jafari@gmail.com>
2020-09-08 02:12:15 +0430

    Add Persian translation

---
+++ translations/fa.js
@@ -0,0 +1,343 @@
+var fa = {
+  audio: "صدا",
+
+  video: "ویدیو",
+
+  playerHeading: "پخش‌کننده رسانه",
+
+  faster: "سریع‌تر",
+
+  slower: "آهسته‌تر",
+
+  play: "پخش",
+
+  pause: "مکث",
+
+  stop: "توقف",
+
+  restart: "راه‌اندازی مجدد",
+
+  prevChapter: "قسمت قبلی",
+
+  nextChapter: "قسمت بعدی",
+
+  prevTrack: "آهنگ قبلی",
+
+  nextTrack: "آهنگ بعدی",
+
+  rewind: "به عقب",
+
+  forward: "به جلو",
+
+  captions: "زیرنویس‌ها",
+
+  showCaptions: "نمایش زیرنویس‌ها",
+
+  hideCaptions: "پنهان‌کردن زیرنویس‌ها",
+
+  captionsOff: "خاموش‌کردن زیرنویس‌ها",
+
+  showTranscript: "نمایش متن رونوشت",
+
+  hideTranscript: "پنهان‌کردن متن رونوشت",
+
+  turnOnDescriptions: "روشن‌کردن توضیحات",
+
+  turnOffDescriptions: "خاموش‌کردن توضیحات",
+
+  chapters: "قسمت‌ها",
+
+  newChapter: "قسمت جدید",
+
+  language: "زبان",
+
+  sign: "زبان اشاره",
+
+  showSign: "نمایش زبان اشاره",
+
+  hideSign: "پنهان‌کردن زبان اشاره",
+
+  seekbarLabel: "خط‌زمانی",
+
+  mute: "صدا دار",
+
+  unmute: "قطع صدا",
+
+  volume: "صدا",
+
+  volumeHelp: "برای دسترسی به اسلایدر میزان صدا کلیک کنید",
+
+  volumeUpDown: "افزایش کاهش صدا",
+
+  volumeSliderClosed: "اسلایدر صدا بسته شد",
+
+  preferences: "تنظیمات",
+
+  enterFullScreen: "ورود به تمام صفحه",
+
+  exitFullScreen: "خروج از تمام صفحه",
+
+  fullScreen: "تمام صفحه",
+
+  speed: "سرعت",
+
+  and: "و",
+
+  or: "یا",
+
+  spacebar: "فاصله",
+
+  transcriptTitle: "متن رونوشت",
+
+  lyricsTitle: "متن آهنگ",
+
+  autoScroll: "اسکرول خودکار",
+
+  unknown: "ناشناخته",
+
+  statusPlaying: "در حال پخش",
+
+  statusPaused: "مکث شده",
+
+  statusStopped: "متوقف شده",
+
+  statusWaiting: "در انتظار",
+
+  statusBuffering: "در حال بافر کردن",
+
+  statusUsingDesc: "با استفاده از نسخه توصیف شده",
+
+  statusLoadingDesc: "در حال بارگذاری نسخه توصیف شده",
+
+  statusUsingNoDesc: "در حال استفاده از نسخه غیر توصیف شده",
+
+  statusLoadingNoDesc: "در حال بارگذاری نسخه غیر توصیف شده",
+
+  statusLoadingNext: "در حال بارگذاری آهنگ بعدی",
+
+  statusEnd: "پایان آهنگ",
+
+  selectedTrack: "آهنگ انتخاب شده",
+
+  alertDescribedVersion: "با استفاده از نسخه توصیف شده صوتی این ویدیو",
+
+  alertNonDescribedVersion: "با استفاده از نسخه غیر توصیف شده صوتی این ویدیو",
+
+  fallbackError1: "متاسفیم، مرورگر شما قادر به پخش این مورد نیست",
+
+  fallbackError2: "مرورگرهای زیر برای کار با این پخش‌کنند رسانه شناخته شده‌اند",
+
+  orHigher: "یا بالاتر",
+
+  prefMenuCaptions: "زیرنویس‌ها",
+
+  prefMenuDescriptions: "توضیحات",
+
+  prefMenuKeyboard: "صفحه کلید",
+
+  prefMenuTranscript: "متن رونوشت",
+
+  prefTitleCaptions: "تنظیمات زیرنویس‌ها",
+
+  prefTitleDescriptions: "تنظیمات توضیحات صوتی",
+
+  prefTitleKeyboard: "تنظیمات صفحه کلید",
+
+  prefTitleTranscript: "تنظیمات متن رونوشت",
+
+  prefIntroCaptions: "تنظیمات زیر نحوه نمایش زیرنویس‌ها را کنترل می‌کند.",
+
+  prefIntroDescription1:
+    "این پخش‌کنند رسانه به دو روش از توضیحات صوتی پشتیبانی می‌کند: ",
+
+  prefIntroDescription2: "ویدیوی فعلی دارد ",
+
+  prefIntroDescriptionNone: "ویدیو فعلی هیچ توضیحات صوتی در هر دو قالب ندارد.",
+
+  prefIntroDescription3:
+    "از فرم زیر برای تنظیم ترجیحات مربوط به توضیحات صوتی مبتنی بر متن استفاده کنید.",
+
+  prefIntroDescription4:
+    "پس از ذخیره تنظیمات، می‌توانید توضیحات صوتی را با استفاده از دکمه توضیحات روشن/خاموش تغییر دهید.",
+
+  prefIntroKeyboard1:
+    "پخش‌کنند رسانه این صفحه وب را می توان از هر نقطه در صفحه با استفاده از میانبرهای صفحه کلید کنترل کرد (فهرست پایین را ببینید).",
+
+  prefIntroKeyboard2:
+    "کلیدهای اصلاح کننده (Shift، Alt و Control) را می‌توانید در زیر اختصاص دهید.",
+
+  prefIntroKeyboard3:
+    "توجه: برخی از ترکیب‌های کلید ممکن است با کلیدهای مورد استفاده توسط مرورگر شما و/یا برنامه‌های کاربردی دیگر در تضاد باشند. ترکیب‌های مختلفی از کلیدهای اصلاح کننده را امتحان کنید تا یکی از موارد مناسب شما که کار می‌کند را پیدا کنید.",
+
+  prefIntroTranscript: "تنظیمات زیر روی متن رونوشت تاثیر می‌گذارد.",
+
+  prefCookieWarning: "برای ذخیره تنظیمات شما نیاز به کوکی‌ها داریم.",
+
+  prefHeadingKeyboard1: "کلیدهای اصلاح کنند مورد استفاده برای میانبرها",
+
+  prefHeadingKeyboard2: "میانبرهای صفحه کلید فعلی",
+
+  prefHeadingDescription: "توضیحات صوتی",
+
+  prefHeadingTextDescription: "متن مبتنی بر توضیحات صوتی",
+
+  prefHeadingCaptions: "زیرنویس‌ها",
+
+  prefHeadingTranscript: "متن رونوشت تعاملی",
+
+  prefAltKey: "Alt",
+
+  prefCtrlKey: "Control",
+
+  prefShiftKey: "Shift",
+
+  escapeKey: "Escape",
+
+  escapeKeyFunction: "بستن پنجره گفتگوی فعلی یا منوی بازشو",
+
+  prefDescFormat: "تنظیمات قالب",
+
+  prefDescFormatHelp: "اگر هر دو قالب موجود باشد، فقط یکی استفاده می‌شود.",
+
+  prefDescFormatOption1: "جایگزین نسخه توصیف شده ویدیو",
+
+  prefDescFormatOption1b: "یک نسخه توصیف شده جایگزین",
+
+  prefDescFormatOption2: "توضیحات مبتنی بر متن، اعلام شده توسط صفحه‌خوان",
+
+  prefDescFormatOption2b: "توضیحات مبتنی بر متن",
+
+  prefDescPause: "هنگام شروع توضیحات، ویدیو را به طور خودکار مکث کن",
+
+  prefVisibleDesc: "توضیحات را قابل مشاهده کنید",
+
+  prefHighlight: "متن رونوشت را در هنگام پخش رسانه برجسته می‌کند",
+
+  prefTabbable: "فعال کردن صفحه کلید متن رونوشت",
+
+  prefCaptionsFont: "قلم",
+
+  prefCaptionsColor: "رنگ متن",
+
+  prefCaptionsBGColor: "پس‌زمینه",
+
+  prefCaptionsSize: "اندازه قلم",
+
+  prefCaptionsOpacity: "شفافیت",
+
+  prefCaptionsStyle: "سبک",
+
+  serif: "serif",
+
+  sans: "sans-serif",
+
+  cursive: "cursive",
+
+  fantasy: "fantasy",
+
+  monospace: "monospace",
+
+  white: "سفید",
+
+  yellow: "زرد",
+
+  green: "سبز",
+
+  cyan: "فیروزه‌ای",
+
+  blue: "آبی",
+
+  magenta: "ارغوانی",
+
+  red: "قرمز",
+
+  black: "سیاه",
+
+  transparent: "شفاف",
+
+  solid: "مستحکم",
+
+  captionsStylePopOn: "بازشو",
+
+  captionsStyleRollUp: "چرخیدن",
+
+  prefCaptionsPosition: "موقعیت",
+
+  captionsPositionOverlay: "هم‌پوشانی",
+
+  captionsPositionBelow: "در زیر ویدیو",
+
+  sampleCaptionText: "متن نمونه زیرنویس",
+
+  prefSuccess: "تغییرات شما ذخیره شد.",
+
+  prefNoChange: "شما هیچ تغییری ایجاد نکردید.",
+
+  help: "کمک",
+
+  helpTitle: "کمک",
+
+  save: "ذخیره",
+
+  cancel: "انصراف",
+
+  ok: "باشه",
+
+  done: "انجام شد",
+
+  closeButtonLabel: "بستن گفتگو",
+
+  windowButtonLabel: "گزینه‌های پنجره",
+
+  windowMove: "حرکت",
+
+  windowMoveAlert:
+    "برای جابجایی پنجره‌، بکشید پنجره را یا از کلیدهای جهت‌دار استفاده کنید؛ برای متوقف کردن Enter بزنید",
+
+  windowResize: "تغییر اندازه",
+
+  windowResizeHeading: "پنجره تغییر اندازه",
+
+  windowResizeAlert: "اندازه پنجره تغییر کرده است.",
+
+  windowClose: "بستن",
+
+  width: "عرض",
+
+  height: "ارتفاع",
+
+  windowSendBack: "ارسال به عقب",
+
+  windowSendBackAlert: "این پنجره در حال حاضر پشت اشیاء دیگر در صفحه است.",
+
+  windowBringTop: "به جلو بیاورید",
+
+  windowBringTopAlert: "این پنجره در حال حاضر در مقابل اشیاء دیگر در صفحه است.",
+
+  resultsSummary1: "شما جستجو کردید:",
+
+  resultsSummary2: "پیدا شد",
+
+  resultsSummary3: "موارد منطبق",
+
+  resultsSummary4:
+    "برای پخش ویدیو از آن زمان، بر روی زمان مرتبط با هر مورد کلیک کنید.",
+
+  noResultsFound: "هیچ نتیجه‌ای پیدا نشد.",
+
+  searchButtonLabel: "پخش در",
+
+  hour: "ساعت",
+
+  minute: "دقیقه",
+
+  second: "ثانیه",
+
+  hours: "ساعت",
+
+  minutes: "دقیقه",
+
+  seconds: "ثانیه",
+
+  enableKeyboardShortcuts: "فعال کردن میانبرهای صفحه کلید",
+};
